80efbdad6bdf0d8b5135ed921da44a2d2c90f64f
Ritesh-Kumar <riteshkr198312@gmail.com>
2013-10-13 17:32:31 +0530

    Added more beautification to scaffold pages

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,6 +1,6 @@
 # encoding : utf-8
 class PoliciesController < BeautifulController
-
+  before_filter :authenticate_user!
   before_filter :load_policy, :only => [:show, :edit, :update, :destroy]
 
   # Uncomment for check abilities with CanCan
